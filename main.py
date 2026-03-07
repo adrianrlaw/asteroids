b4fa8ae1777b1a8080ab5ff5223e22993ada3b6f
Adrian R. Law <adrianrlaw12@gmail.com>
2026-03-07 14:07:34 -0800

    Initial CircleShape and Player object

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -11,6 +12,10 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -23,8 +28,10 @@ def main():
                     pass
 
         screen.fill("black")
+        player.draw(screen)
 
         pygame.display.flip()
+        dt = clock.tick(60)
 
 
 if __name__ == "__main__":
